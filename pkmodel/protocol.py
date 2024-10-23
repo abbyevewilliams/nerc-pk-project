3d94a165163dbbca1818249178789cc6357d3a4b
Lucy Wright <sedm7478@ox.ac.uk>
2024-10-23 16:39:21 +0100

    set up dosage protocal and test_protocal file

--- pkmodel/protocol.py
+++ pkmodel/protocol.py
@@ -8,10 +8,24 @@ class Protocol:
     Parameters
     ----------
 
-    value: numeric, optional
-        an example paramter
+    dose: string, ['intravenous bolus', 'subcutaneous']
 
     """
-    def __init__(self, value=43):
-        self.value = value
+class Protocol:
+
+    def __init__(self):
+        self.dose = 'intra'
+
+    def __str__(self): 
+        return self.dose
+    
+    def set_dose(self, type):
+        valid_types =['intra', 'subc']
+        if type not in valid_types:
+            raise ValueError(f"Invalid dose type. Choose from: {valid_types}")
+        self.dose = type
+        return f"Dose set to: {type}"
+
+
+
 
--- pkmodel/tests/test_protocol.py
+++ pkmodel/tests/test_protocol.py
@@ -1,15 +1,37 @@
 import unittest
 import pkmodel as pk
+import pytest
 
 
 class ProtocolTest(unittest.TestCase):
     """
     Tests the :class:`Protocol` class.
     """
-    def test_create(self):
+    def test_dosage(self):
         """
-        Tests Protocol creation.
+        Tests Protocol dosage default.
         """
-        model = pk.Protocol()
-        self.assertEqual(model.value, 43)
+        protocol_test = pk.Protocol()
+        protocol_test.set_dose()
+        self.assertEqual(protocol_test.dose, 'intra')
 
+    def test_dosage2(self):
+        """
+        Tests Protocol set_dose for 'intra'.
+        """
+        protocol_test = pk.Protocol()
+        protocol_test.set_dose('insta')
+        self.assertEqual(protocol_test.dose, 'intra')
+
+    def test_dosage3(self):
+        """
+        Tests Protocol set_dose for 'subc'.
+        """
+        protocol_test = pk.Protocol()
+        protocol_test.set_dose('subc')
+        self.assertEqual(protocol_test.dose, 'subc')
+
+    def test_invalid_type_fails(self):
+        protocol_test = pk.Protocol
+        with pytest.raises(Exception) as e_info:
+         protocol_test.set_dose('invalid_type')
