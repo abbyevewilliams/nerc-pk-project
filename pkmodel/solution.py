3c6c6c605c193fc0689e7a773b03fe218f26517f
taylorm2024 <m.r.taylor135@gmail.com>
2024-10-24 15:27:45 +0100

    Added text outlining the next steps to develop this file.

--- pkmodel/solution.py
+++ pkmodel/solution.py
@@ -1,5 +1,14 @@
 #Script to run the model and then calculate and plot the output
 
+"""
+More work needed on this file. The next step would be to add in the values of parameters of Model in line 14, 
+check the output of protocol.py and ensure the qp and qc outputs are arrays of the same length as the time
+array defined in model.py
+
+Finally should make a test_solutions.py file to make sure the steps are running as intended. 
+"""
+
+
 from pkmodel.model import Model
 from pkmodel.protocol import Protocol
 from pkmodel.plot_function import plot_data
