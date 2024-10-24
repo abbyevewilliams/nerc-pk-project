f263c92a260337246169841a65336196d1eeb763
taylorm2024 <m.r.taylor135@gmail.com>
2024-10-24 15:10:25 +0100

    Removed the reference to solutions class which does not exist. Added .plot_function

--- pkmodel/__init__.py
+++ pkmodel/__init__.py
@@ -10,4 +10,5 @@ from .version_info import VERSION_INT, VERSION  # noqa
 # Import main classes
 from .model import Model    # noqa
 from .protocol import Protocol    # noqa
-from .solution import Solution     # noqa
+
+from .plot_function import plot_data
